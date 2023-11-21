d829851cd3eb6de6a51b8a765537b9ddbac4fea2
dishakemble260 <101816297+dishakemble260@users.noreply.github.com>
2023-11-21 23:59:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import nodemon from 'nodemon';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import connectDB from '../config/db.js';
-import authRoute from '../routes/authRoute.js'
-import categoryRoutes from '../routes/categoryRoutes.js'
-import productRoutes from "../routes/productRoutes.js";
+import connectDB from './config/db.js';
+import authRoute from './routes/authRoute.js'
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
 
